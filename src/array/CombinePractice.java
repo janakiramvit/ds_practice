466268498461607c1ee4ea6d91e6546c4ca8c11a
Janakiram Pulipati <jpulipati@expedia.com>
2017-08-12 11:21:05 -0700

    First commit

--- src/array/CombinePractice.java
+++ src/array/CombinePractice.java
@@ -8,7 +8,7 @@ import java.util.List;
  */
 
 /*My Approach during the interview:
-    * backtracking is the way to go
+    * backtracking is the way to go for this
     * I started out with having a recursion within a 'for' loop to achieve backtracking
     * Got digressed later thinking about how to add elements to arraylist and list subsequently
 */
@@ -23,10 +23,10 @@ public class CombinePractice {
     }
 
     public void backTrack(List<List<Integer>> list, List<Integer> temp,int start, int n[]){
-        list.add(new ArrayList<>(temp));        //Add array values to the list - David was trying to help me with this. e.g:list = {{},{1} ..}
-        for(int i = start;i<n.length;i++){      //Starts with 0 and increments till length
-            temp.add(n[i]);                     //Adds array values to array list e.g: temp = {1}
-            backTrack(list,temp, i+1,n);        //Increment the 'start' value during recursion to attach consecutive values to existing values
+        list.add(new ArrayList<>(temp));        //Add new array-values to the 'list' - David was trying to help me with this. e.g:list = {{},{1} ..}
+        for(int i = start;i<n.length;i++){
+            temp.add(n[i]);                     //Adds input array values to array list e.g: temp = {1}
+            backTrack(list,temp, i+1,n);        //Start recursion, Increment the 'start' value i+1, to attach consecutive values to existing values
             temp.remove(temp.size()-1);         //Removes old values in temp through the recursion
         }
     }
