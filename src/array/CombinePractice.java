aff45b67fcbfe26ab47de45d6bee14447852ae76
Janakiram Pulipati <jpulipati@expedia.com>
2017-08-12 11:32:44 -0700

    First commit

--- src/array/CombinePractice.java
+++ src/array/CombinePractice.java
@@ -18,22 +18,29 @@ public class CombinePractice {
     public List<List<Integer>> combine(int[] n){
         List<List<Integer>> list = new ArrayList<>();
         backTrack(list, new ArrayList<>(),0,n);
-        list.remove(0);                         //Remove blank array list at the beginning of the list
+        list.remove(0);                         //Remove blank array list thats present at the beginning of the final result list
         return list;
     }
 
     public void backTrack(List<List<Integer>> list, List<Integer> temp,int start, int n[]){
-        list.add(new ArrayList<>(temp));        //Add new arraylist to the 'list' - David was trying to help me with this. e.g:list = {{},{1} ..}
+        list.add(new ArrayList<>(temp));        //Add new 'arraylist' to final result 'list'; add 'values' to this arraylist; e.g:list = {{},{1} ..}
+                                                //David was trying to help me with this.
         for(int i = start;i<n.length;i++){
             temp.add(n[i]);                     //Adds 'input array values' to 'temp' arraylist e.g: temp = {1}
-            backTrack(list,temp, i+1,n);        //Start recursion, Increment the 'start' value i+1, to attach consecutive values to existing values
+            backTrack(list,temp, i+1,n);        //Start recursion; Increment the 'start' value i+1, to attach consecutive values to existing values
             temp.remove(temp.size()-1);         //Removes old values in temp through the recursion
         }
     }
 
     public static void main(String[] args){
         CombinePractice combinePractice = new CombinePractice();
-        int[] n = {1,2};                        //input array
+        int[] n = {-3,2};                        //input array
+        /*Test cases include:
+        * Ideal case: n = {1,2}
+        * All zeroes: n = {0,0}
+        * Negatives: n = {-1,-2}
+        * Test 'size' of the array with larger array size
+        * */
         System.out.println(combinePractice.combine(n));
     }
 }
